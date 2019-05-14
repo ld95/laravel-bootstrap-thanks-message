e80094e5806f64fac9d7e2e0470a46176f07052c
Yansong Liu <yl2223@njit.edu>
2019-05-14 00:14:14 -0400

    Error 2: Tests\Unit\UserTest::testInsert Illuminate\Database\QueryException: SQLSTATE[23000]: Integrity constraint violation: 19 UNIQUE constraint failed: users.email (SQL: insert into "users" ("name", "email", "email_verified_at", "updated_at", "created_at", "password", "remember_token") values (Song, song@example.com, 2019-05-13 15:47:59, 2019-05-13 15:47:59, 2019-05-13 15:47:59, 11223344aabbccdd, 7sGmmdyqJy))

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -16,8 +16,8 @@ class UserTest extends TestCase
      */
     public function testInsert(){
         $user = new User();
-        $user->name = 'Yansong Liu';
-        $user->email = 'yl2223@example.com';
+        $user->name = 'Song';
+        $user->email = 'song@example.com';
         $user->email_verified_at = '2019-05-13 15:47:59';
         $user->updated_at = '2019-05-13 15:47:59';
         $user->created_at = '2019-05-13 15:47:59';
@@ -25,6 +25,7 @@ class UserTest extends TestCase
         $user->remember_token ='7sGmmdyqJy';
 
         $this->assertTrue($user->save());
+        
 
     }
 
